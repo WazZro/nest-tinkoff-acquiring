c2abba613a078ccf038e9967bdadc9d2f0ff2f74
Lirael <wazzroboy@gmail.com>
2020-02-25 05:17:43 -0500

    style(url-enum): single quote

--- lib/url.enum.ts
+++ lib/url.enum.ts
@@ -1,8 +1,8 @@
 export enum TinkoffAcquiringUrls {
-  INIT = "https=//securepay.tinkoff.ru/rest/Init",
-  CONFIRM = "https=//securepay.tinkoff.ru/rest/Confirm",
-  CHARGE = "https=//securepay.tinkoff.ru/rest/Charge",
-  CANCEL = "https=//securepay.tinkoff.ru/rest/Cancel",
-  GET_STATE = "https=//securepay.tinkoff.ru/rest/GetState",
-  RESEND = "https=//securepay.tinkoff.ru/rest/Resend"
+  INIT = 'https://securepay.tinkoff.ru/v2/Init',
+  FINISH_AUTHORIZE = 'https://securepay.tinkoff.ru/v2/FinishAuthorize',
+  CONFIRM = '	https://securepay.tinkoff.ru/v2/Confirm',
+  CANCEL = 'https://securepay.tinkoff.ru/v2/Cancel',
+  GET_STATE = 'https://securepay.tinkoff.ru/v2/GetState',
+  RESEND = 'https://securepay.tinkoff.ru/v2/Resend',
 }
