dd20d507cbb56fce100b4b79294851aefe882981
Вадим Усинов <1175zolotoykod.ru>
2020-03-03 07:16:25 -0500

    fix(index): export statuses enum

--- lib/index.ts
+++ lib/index.ts
@@ -3,12 +3,31 @@ import { AcquiringModule } from './acquiring.module';
 import { AcquiringService } from './acquiring.service';
 import { OptionsBase, ResponseBase } from './interfaces/base.interface';
 import { CancelOptions, CancelResponse } from './interfaces/cancel.interface';
-import { ConfirmOptions, ConfirmResponse } from './interfaces/confirm.interface';
-import { FinishAuthorizeOptions, FinishAuthorizeResponse } from './interfaces/finish-authorize.interface';
+import {
+  ConfirmOptions,
+  ConfirmResponse,
+} from './interfaces/confirm.interface';
+import {
+  FinishAuthorizeOptions,
+  FinishAuthorizeResponse,
+} from './interfaces/finish-authorize.interface';
 import { InitOptions, InitResponse } from './interfaces/init.interface';
 import { StateOptions, StateResponse } from './interfaces/state.interface';
+import { TinkoffAcquiringStatuses } from './status.enum';
 
 export { AcquiringModule, AcquiringService, AcquiringOptionsService };
-export { OptionsBase, ResponseBase, InitOptions, InitResponse, CancelOptions, CancelResponse, StateOptions, StateResponse, ConfirmOptions, ConfirmResponse, FinishAuthorizeOptions, FinishAuthorizeResponse, };
-
-
+export {
+  OptionsBase,
+  ResponseBase,
+  InitOptions,
+  InitResponse,
+  CancelOptions,
+  CancelResponse,
+  StateOptions,
+  StateResponse,
+  ConfirmOptions,
+  ConfirmResponse,
+  FinishAuthorizeOptions,
+  FinishAuthorizeResponse,
+};
+export { TinkoffAcquiringStatuses };
